f8739bd407afd1177adad9eeab8bec3c4eb7dcd8
guo543 <guo543@purdue.edu>
2021-12-02 15:37:01 -0500

    added logo to main menu

--- src/main/java/edu/purdue/view/MenuPanel.java
+++ src/main/java/edu/purdue/view/MenuPanel.java
@@ -21,6 +21,9 @@ public class MenuPanel extends JPanel {
         greeting.setBounds(570, 150, 200, 20);
         greeting.setFont(new Font("Times New Roman", Font.PLAIN, 14));
 
+        ImageIcon logo = new ImageIcon("images/logo.png");
+        JLabel logoLabel = new JLabel(logo);
+
         JLabel title = new JLabel("<html>CONSTRICTED CONSTRICTOR</html>");
         title.setForeground(new Color(7, 2, 175));
         title.setFont(new Font("Gill Sans Ultra Bold", Font.BOLD, 35));
@@ -37,7 +40,7 @@ public class MenuPanel extends JPanel {
         special_thanks = new JButton("SPECIAL THANKS");
         special_thanks.setPreferredSize(new Dimension(200,50));
         this.add(title);
-        title.setBounds(65, 150, 800, 200);
+        title.setBounds(65, 180, 800, 200);
 
         this.add(start);
         start.setBounds(288, 360, 200, 50);
@@ -56,6 +59,9 @@ public class MenuPanel extends JPanel {
 
         this.add(special_thanks);
         special_thanks.setBounds(288, 660, 200, 50);
+
+        this.add(logoLabel);
+        logoLabel.setBounds(288, 70, 198, 198);
     }
 
     public void paintComponent(Graphics g) {
